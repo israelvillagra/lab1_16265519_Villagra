207d03b006bc9259ef51b11103854e7a5ad1ce1a
israelvillagra <israel.villagra@usach.cl>
2020-09-12 23:54:46 -0300

    Impresión
    Impresión de instrucciones

--- Lab1.rkt
+++ Lab1.rkt
@@ -68,17 +68,17 @@ remoteRepositorylist
               )
 )
 
-(define (f1 x y)
-  (+ (* 2 (expt x 2)) (* 3 y) 1))
-(define (f2 x y)
-  (+ (* x y) 1))
+;(define (f1 x y)
+;  (+ (* 2 (expt x 2)) (* 3 y) 1))
+;(define (f2 x y)
+;  (+ (* x y) 1))
 
 (define named-functions
   (list (cons add add)
         (cons pull pull)
-        (cons "three" (lambda (x y) (/ (f1 x y) (f2 x y))))
         (cons commit commit)
         (cons push push)
+        
   )
 )
 
@@ -103,6 +103,39 @@ remoteRepositorylist
         (cdr p)
         (error "Función no encontrada"))))
 
+
+
+
+(define (workspace->string lista)
+               lista              
+)
+
+(define (index->string lista)
+               lista              
+)
+
+(define (localRepsitory->String lista)
+               lista              
+)
+
+(define (remoteRepository->String zona)
+               zona              
+)
+
+(define zonasString
+  (list (cons workspace workspace->string)
+        (cons index  index->string)
+        (cons localRepository localRepsitory->String)
+        (cons remoteRepository remoteRepository->String)
+  )
+)
+
+(define (zonas->string name)
+  (let ((p (assoc name zonasString)))
+    (if p
+        (cdr p)
+        (error "Función no encontrada"))))
+
 ;((name->function "three") 4 5)
 ;((git add) 4)
 ;(((git add) (list “file1.rkt” “file2.rkt”)) zonas)
@@ -119,20 +152,35 @@ remoteRepositorylist
 ;(((git add) (list "file1.rkt" "file2.rkt")) localRepository) ; Ingresa elementos a LocalRepository
 ;(((git add) (list "file1.rkt" "file2.rkt")) remoteRepository) ; Ingresa elementos a RemoteRepository
 
-(write "Se agregan elementos al Workspace")
+
+; Se agregan elementos en workSpace
 (((git add) (list "file1.rkt" "file2.rkt" "file3.rkt" "file4.rkt")) workspace)
 
-(write "Se agregan elementos a Workspace, que son enviados a la zona Index")
 ;Se agregan elementos a Workspace, que son enviados a la zona Index
 (((git add) (((git add) (list "file1.rkt" "file2.rkt")) workspace) ) index)
 
-(write "Se agregan del Index hacia Local Repository")
+;Se agregan del Index hacia Local Repository
 (((git commit) "Primer Commit") (((git add) (((git add) (list "file1.rkt" "file2.rkt")) workspace) ) index))
 
-(write "Se ingresan todos los elementos dentro de la Zona de trabajo Local Repository a Remote Repository")
+;Se ingresan todos los elementos dentro de la Zona de trabajo Local Repository a Remote Repository
 ((git push) (((git commit) "Primer Commit") (((git add) (((git add) (list "file1.rkt" "file2.rkt")) workspace) ) index)))
 
 ;Ejemplo tomado de los videos campus virtual
 (define suma (lambda (a) (lambda(b) (+ a b))))
 
-(((git commit) "Primer Commit") (((git add) (((git add) (list "file1.rkt" "file2.rkt")) workspace) ) index))
+
+(write "Archivos de Workspace")
+((zonas->string workspace) (((git add) (list "file1.rkt" "file2.rkt" "file3.rkt" "file4.rkt")) workspace))
+
+(write "Archivos de Index")
+(((git add) (((git add) (list "file1.rkt" "file2.rkt")) workspace) ) index)
+
+(write "Commits Dentro de Local Repository")
+((zonas->string localRepository) (list (cdr (((git commit) "Primer Commit") (((git add) (((git add) (list "file1.rkt" "file2.rkt")) workspace) ) index)))
+        (cdr (((git commit) "Segundo Commit") (((git add) (((git add) (list "file1.rkt" "file2.rkt")) workspace) ) index)))
+        )
+)
+
+(write "Elementos Dentro de Remote Repository")
+((zonas->string remoteRepository) ((git push) (((git commit) "Primer Commit") (((git add) (((git add) (list "file1.rkt" "file2.rkt")) workspace) ) index)))
+)
